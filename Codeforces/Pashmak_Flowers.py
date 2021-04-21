4c5c6d8a072e149c2b7819c148ea454685b69da0
Binayak Bishnu <71837066+BinayakBishnu@users.noreply.github.com>
2021-04-21 07:56:28 +0530

    Update Pashmak_Flowers.py

--- Codeforces/Pashmak_Flowers.py
+++ Codeforces/Pashmak_Flowers.py
@@ -8,14 +8,10 @@ L1 = L
 L1.sort()
 m = abs(L1[number-1]- L1[number-2])
 
-c=-1
-
+c=0
 for i in range(number-1):
     for j in range(i+1,number):
-        if m<abs(L[i]-L[j]):
-            m = abs(L[i]-L[j])
-            c=1
-        elif m == abs(L[i] - L[j]):
+        if abs(L[i]-L[j]) == x:
             c+=1
-            
+
 print(x,c)
