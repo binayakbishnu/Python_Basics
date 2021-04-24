b958597328adb713c3a232c6a07cae4a3f16e3a8
Binayak Bishnu <71837066+BinayakBishnu@users.noreply.github.com>
2021-04-24 20:58:28 +0530

    Update leonard's prime numbers.py

--- Miscellaneous-Py/leonard's prime numbers.py
+++ Miscellaneous-Py/leonard's prime numbers.py
@@ -1,3 +1,10 @@
+# take q input for number of cases
+# for each q take input n
+# find the maximum number of distinct prime factors a number in range
+# 1 to n (inclusive) can have
+# Example: for n = 500
+# the maximum number of unique prime factors a number from 1 to 500 can have is 4
+
 def primeCount(n):
     
     prime=[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
